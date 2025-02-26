ef263c3a95ea07f437c20e8965cf0411cf40b546
akshay <akshayffb@gmail.com>
2025-02-26 13:20:06 +0530

    initial commit

---
+++ storage/system/cache/fa4/198/home.blade.php
@@ -0,0 +1,6 @@
+<?php 
+/* C:\Users\Akshay ffb\Desktop\Freelance\restaurant\themes\demo/_pages/home.blade.php */
+class Pagic67bebf665ea80197168518_046866314996a6182f04749e0532af60Class extends \Main\Template\Code\PageCode
+{
+
+}
---
+++ storage/system/cache/fa8/b5b/default.blade.php
@@ -0,0 +1,6 @@
+<?php 
+/* C:\Users\Akshay ffb\Desktop\Freelance\restaurant\themes\demo/_layouts/default.blade.php */
+class Pagic67bec4f85f924802003321_1fd8d11d596ce2e2797b1d164dbfdc0aClass extends \Main\Template\Code\LayoutCode
+{
+
+}
